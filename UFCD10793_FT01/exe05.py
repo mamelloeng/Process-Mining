6df0f3f3eb8ade88a0f032f419e67c442aeaa955
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-15 12:30:38 +0000

    UFCD10793_FT01\exe05.py

---
+++ UFCD10793_FT01/exe05.py
@@ -0,0 +1,5 @@
+a = int(input("Introduza o primeiro cateto do triângulo retângulo\n->"))
+b = int(input("Introduza o segundo cateto do triângulo retângulo\n->"))
+c = (a**2 + b**2)**0.5
+
+print("O valor da hipotenusa do triângulo retângulo com catetos {} e {} é igual a: {}".format(a, b, c))
