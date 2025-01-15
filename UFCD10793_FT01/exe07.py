57ad94d112494e7914cc51c3f37d2502799b3f46
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-15 12:42:45 +0000

    UFCD10793_FT01\exe07.py

---
+++ UFCD10793_FT01/exe07.py
@@ -0,0 +1,5 @@
+km = int(input("Introduza a distância em km\n->"))
+litros = int(input("Introduza o consumo em litros\n->"))
+consumo = km/litros
+
+print("O consumo médio do veículo é de {:.2f} km/l".format(consumo))
