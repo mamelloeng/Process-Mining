94c6df8d44b3826208825ff44b6de5eb4b18a4b8
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-21 10:32:36 +0000

    UFCD10793_FT02\exe04.py

---
+++ UFCD10793_FT02/exe04.py
@@ -0,0 +1,5 @@
+n1 = float(input("Introduzaa primeira nota \n->"))
+n2 = float(input("Introduza a segunda nota \n->"))
+m = (n1 + n2) / 2
+
+print("A média das notas {} e {} é igual a: {:.2f}".format(n1, n2, m))
