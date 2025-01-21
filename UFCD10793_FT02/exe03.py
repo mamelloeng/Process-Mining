c104f743ac91bca1741d4a5b41cba3437f34024e
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-21 10:26:28 +0000

    UFCD10793_FT02\exe03.py

---
+++ UFCD10793_FT02/exe03.py
@@ -0,0 +1,6 @@
+a = int(input("Escreva um número inteiro\n->"))
+b = int(input("Escreva outro número inteiro\n->"))
+c = int(input("Escreva mais um número inteiro\n->"))
+d = a + b + c
+
+print("O resultado de somar {}, {} e {} é igual a: {}".format(a, b, c, d))
