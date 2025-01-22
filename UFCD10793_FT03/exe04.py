6713bdc1ce587c584faec135fa3147da1a3d1fa8
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-22 09:50:46 +0000

    UFCD10793_FT03\exe04.py

---
+++ UFCD10793_FT03/exe04.py
@@ -0,0 +1,9 @@
+n = float(input("Introduza um número\n->"))
+n1 = float(input("Introduza outro número\n->"))
+
+if n > n1:
+    print("O primeiro número é maior que o segundo")
+elif n == n1:
+    print("Os números são iguais")
+else:
+    print("O primeiro número é menor que o segundo")
