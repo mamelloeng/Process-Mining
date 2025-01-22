113d1c94d806924115f3327a87960c1211cd3fbf
mamelloeng <matheuspereiraeng@gmail.com>
2025-01-22 09:30:45 +0000

    UFCD10793_FT03\exe02.py

---
+++ UFCD10793_FT03/exe02.py
@@ -0,0 +1,7 @@
+n = int(input("Escreva um número inteiro\n->"))
+n1 = int(input("Escreva outro número inteiro\n->"))
+
+if n == n1:
+    print("Os números são iguais")
+else:
+    print("Os números são diferentes")
