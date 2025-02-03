52c55993b50aa2b8f62eb5314f3ae5491421c289
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-03 19:51:33 +0000

    adiciona UFCD10793_FT04\exe03.py

---
+++ UFCD10793_FT04/exe03.py
@@ -0,0 +1,8 @@
+n1 = int(input("Escreva um número inteiro e positivo\n->"))
+n2 = int(input("Escreva outro número inteiro e positivo\n->"))
+n3 = int(input("Escreva mais um número inteiro e positivo\n->"))
+n4 = int(input("Escreva outro número inteiro e positivo\n->"))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print("A média dos números {} {} {} e {} é igual a: {}".format(n1, n2, n3, n4, media))
