a6ea45507cb98dce8fb5afad61fa42a247627a42
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-04 09:50:04 +0000

    adiciona UFCD10793_FT04\exe16.py

---
+++ UFCD10793_FT04/exe16.py
@@ -0,0 +1,25 @@
+n = int(input("Introduza um número\n->"))
+
+match n:
+    case 1:
+        print("O número é 1")
+    case 2:
+        print("O número é 2")
+    case 3:
+        print("O número é 3")
+    case 4:
+        print("O número é 4")
+    case 5:
+        print("O número é 5")
+    case 6:
+        print("O número é 6")
+    case 7:
+        print("O número é 7")
+    case 8:
+        print("O número é 8")
+    case 9:
+        print("O número é 9")
+    case 10:
+        print("O número é 10")
+    case _:
+        print("Número inválido")
