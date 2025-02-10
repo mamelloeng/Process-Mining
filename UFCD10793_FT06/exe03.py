3859ab9d4e9399f7e8701e6fd56019fe9c583b4b
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-10 16:16:24 +0000

    adiciona UFCD10793_FT06\exe03.py

--- UFCD10793_FT06/exe03.py
+++ UFCD10793_FT06/exe03.py
@@ -3,4 +3,13 @@ n = 0
 for i in idades:
     if i < 18:
         n+=1
-print(n)
+print("Existem {} pessoas menores de idade".format(n))
+idades.sort(reverse=True)
+print(idades)
+idade = int(input("Digite uma idade: "))
+
+# Verifica se a idade está na lista
+if idade in idades:
+    print("A idade está na lista")
+else:
+    print("Não existe ninguém com essa idade na lista")
