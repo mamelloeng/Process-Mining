a32b3f2e7605677a017d7d3871cae0850a8f3433
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-10 14:29:52 +0000

    adiciona UFCD10793_FT06\exe01.py

---
+++ UFCD10793_FT06/exe01.py
@@ -0,0 +1,13 @@
+cores = ["amarelo", "azul", "branco", "preto", "verde"]
+
+print(cores)
+print(cores[2])
+cores[0] = "vermelho"
+print(cores)
+cores.append("amarelo")
+print(cores)
+cores.insert(2, "roxo")
+print(cores)
+cores.pop()
+print(cores)
+print(len(cores))
