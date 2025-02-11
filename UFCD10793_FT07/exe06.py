e102b707d7789315d974509352c2ec08eda77b90
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-11 21:22:47 +0000

    adiciona UFCD10793_FT07\exe06.py

---
+++ UFCD10793_FT07/exe06.py
@@ -0,0 +1,18 @@
+tuple1 = ('P', 'Y', 'T', 'H', 'O', 'N')
+for i in range(4):
+    print(tuple1[i], end = ",")
+print("\n")
+#print(tuple1[7])
+#print(tuple1[2.0])
+print(tuple1[-1])
+print(tuple1[-2])
+print("\n")
+for i in range(-6, 0):
+    print(tuple1[i], end = ",")
+print("\n")
+tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tuple1[1:5])
+
+print(tuple1[:5])
+print(tuple1[6:])
+print(tuple1[-5:-1])
