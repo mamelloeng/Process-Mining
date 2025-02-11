e99d95c9b0f5e740313be6808d11cbaeeb2ee99a
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-11 20:18:26 +0000

    adiciona UFCD10793_FT07\exe01.py

---
+++ UFCD10793_FT07/exe01.py
@@ -0,0 +1,12 @@
+number_tuple = (10, 20, 25.75)
+print(number_tuple)
+
+string_tuple = ('Jessa', 'Emma', 'Kelly')
+print(string_tuple)
+
+
+sample_tuple = ('Jessa', 30, 45.75, [25, 78])
+print(sample_tuple)
+
+sample_tuple2 = tuple(('Jessa', 30, 45.75, [25, 78]))
+print(sample_tuple2)
