bd957122074f4c38efe8de4fd5d7c67a4afd318c
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-12 11:57:03 +0000

    adiciona UFCD10793_FT07\exe16.py

---
+++ UFCD10793_FT07/exe16.py
@@ -0,0 +1,14 @@
+tuple1 = ('xyz', 'zara', 'abc')
+print(max(tuple1))
+
+print(min(tuple1))
+
+tuple2 = (11, 22, 10, 4)
+
+print(max(tuple2))
+
+print(min(tuple2))
+
+tuple3 = ('a', 'e', 11, 22, 15)
+
+#print(max(tuple3))
