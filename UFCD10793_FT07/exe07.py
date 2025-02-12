0a1e6b0d1a39f467a1cd9dec5e1773134c1e65bd
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-12 09:45:22 +0000

    adiciona UFCD10793_FT07\exe07.py

---
+++ UFCD10793_FT07/exe07.py
@@ -0,0 +1,12 @@
+tuple1 = (10, 20, 30, 40, 50)
+
+position = tuple1.index(30)
+print("Index of 30:", position)
+
+tuple1 = (10, 20, 30, 40, 50, 60, 70, 80)
+
+position = tuple1.index(60, 4, 6)
+print("Index of 60:", position)
+
+position = tuple1.index(10)
+print("Index of 10:", position)
