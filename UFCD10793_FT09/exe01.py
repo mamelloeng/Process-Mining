2d8bd695dc9941a5c0bd654374a1dc62d7d41595
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-13 21:25:00 +0000

    adiciona UFCD10793_FT09\exe01.py

---
+++ UFCD10793_FT09/exe01.py
@@ -0,0 +1,15 @@
+menu={
+"entremeada": 7,
+"Sardinha": 6,
+"Filetes": 5.5,
+"Prego": 7,
+"Hamburguer": 5.5
+}
+
+x = menu["Prego"]
+print(x)
+
+print(menu.keys())
+
+menu["Omelete"] = 5
+print(menu)
