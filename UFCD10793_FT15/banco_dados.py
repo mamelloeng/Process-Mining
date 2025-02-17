c7916f4c2638f1f4c8ca6ee1e475c02526b9f310
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-17 14:28:37 +0000

    adiciona UFCD10793_FT15

--- UFCD10793_FT15/banco_dados.py
+++ UFCD10793_FT15/banco_dados.py
@@ -3,14 +3,24 @@ import sqlite3
 conn = sqlite3.connect('empresa.db')
 cursor = conn.cursor()
 
-cursor.execute("""
+'''cursor.execute("""
      CREATE TABLE funcionarios (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         cargo TEXT NOT NULL,
         salario REAL NOT NULL
      )
-""")
+""") '''
+
+conn.commit()
+conn.close()
+
+conn = sqlite3.connect('empresa.db')
+cursor = conn.cursor()
+
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Ana Silva', 'Gestora', 3500)")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Pedro Santos', 'Programador', 2500)")
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES ('Mariana Costa', 'Designer', 3000)")
 
 conn.commit()
 conn.close()
