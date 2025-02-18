7bde41ce5b327b3c73d1e2f7ad2ac92d1d937294
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-18 20:24:54 +0000

    adiciona UFCD10793_FT13

--- UFCD10793_FT13/exe01.py
+++ UFCD10793_FT13/exe01.py
@@ -1,1 +1,17 @@
-#teste
+import sys
+item = 22
+try:
+    divisao = 1/item
+    print("Valor:", item)
+except (TypeError):
+    print("Você deve digitar apenas números!")
+    print()
+except (ZeroDivisionError):
+    print("Não é possível dividir por zero.")
+    print()
+except:
+    print("Ocorreu a exceção", sys.exc_info()[0])
+else:
+    print("1 dividido por", item, "é", divisao)
+finally:
+    print("O valor fornecido foi:", item)
