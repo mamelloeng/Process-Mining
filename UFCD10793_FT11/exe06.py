1c45aeaa03658e9ffa67b4025963bad1349f2343
mamelloeng <matheuspereiraeng@gmail.com>
2025-02-18 13:33:06 +0000

    adiciona UFCD10793_FT11\exe06.py

---
+++ UFCD10793_FT11/exe06.py
@@ -0,0 +1,8 @@
+def m_por_extenso(n):
+    meses_do_ano = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ]
+    print(meses_do_ano[n-1])
+
+m_por_extenso(1)
