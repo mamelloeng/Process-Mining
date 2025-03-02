80ec58ce09b0c6c86748ee2b30a76eb40133c517
mamelloeng <matheuspereiraeng@gmail.com>
2025-03-02 12:25:59 +0000

    adiciona UFCD10793_FT14

--- UFCD10793_FT14/exe01.py
+++ UFCD10793_FT14/exe01.py
@@ -5,3 +5,18 @@ matrix = np.array([[1,2,3],[4,5,6]])
 print(obj)
 print("\n")
 print(matrix)
+
+print("Número de dimensões do vetor obj: ",obj.ndim)
+print("Número de dimensões da matrix matrix: ",matrix.ndim)
+
+print("Tamanho do vetor obj: ",obj.size)
+print("Tamanho da matrix matrix: ",matrix.size)
+
+print("Forma do vetor obj: ",obj.shape)
+print("Forma da matrix matrix: ",matrix.shape)
+
+print("Tipo do vetor obj: ",type(obj))
+print("Tipo da matrix matrix: ",type(matrix))
+
+print(np.ones((10,2)))
+print(np.zeros((5,4)))
