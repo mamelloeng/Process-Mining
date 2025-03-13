4f0cdc2ebeccaf7d99dcb8b864b56c4493aa6448
mamelloeng <matheuspereiraeng@gmail.com>
2025-03-13 15:19:32 +0000

    adiciona UFCD10793_FT16

--- UFCD10793_FT16/exe01.py
+++ UFCD10793_FT16/exe01.py
@@ -3,3 +3,23 @@ import pandas as pd
 casts = pd.read_csv('cast.csv', index_col=None)
 
 print(casts.describe())
+
+print(casts.head())
+
+titles = pd.read_csv('titles.csv', index_col =None)
+
+print(titles.tail())
+
+pd.set_option('display.max_rows', 10, 'display.max_columns', 10)
+
+print(titles)
+
+tamanho=len(titles)
+
+print(tamanho)
+
+print(titles.head(3))
+
+t = titles['title']
+
+print(type(t))
